2941f6345108cf031d1a5b6ad979c585b7aef9ff
dbeccaria826 <beccdaniel@gmail.com>
2020-05-30 15:01:14 -0400

    added a TechListModal component

---
+++ src/components/techs/TechListModal.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import TechItem from './TechItem';
+import PreLoader from '../layout/PreLoader';
+
+const TechListModal = () => {
+    const [techs, setTechs] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        getTechs();
+        // eslint-disable-next-line
+    }, []);
+
+    const getTechs = async () => {
+        setLoading(true);
+        const res = await fetch('/techs');
+        const data = await res.json();
+
+        setTechs(data);
+        setLoading(false);
+    };
+    if (loading) {
+        return <PreLoader />
+    }
+    return (
+        <div id="tech-list-modal" className="modal">
+            <div className="modal-content">
+                <h4>Technician List</h4>
+                <ul className="collection-with-header">
+                    {!loading && techs.map(tech => <li className="collection-item">{tech.firstName}</li>)}
+                </ul>
+            </div>
+        </div>
+      
+    );
+};
+
+export default TechListModal;
